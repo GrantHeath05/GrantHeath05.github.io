d2e3c4d07b4e1c4568e18c0b1197d03c9620aade
GrantHeath05 <156680651+GrantHeath05@users.noreply.github.com>
2025-06-11 20:23:00 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// js/main.js
-window.addEventListener("DOMContentLoaded", () => {
-  fetch("/components/header.html")
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("header").innerHTML = data;
-    })
-    .catch(err => console.error("Failed to load header:", err));
-});

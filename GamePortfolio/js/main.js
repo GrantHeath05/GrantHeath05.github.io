f487fd0fe8c844fb06d1342dd2f4a49d22ca5251
GrantHeath05 <156680651+GrantHeath05@users.noreply.github.com>
2025-09-03 21:38:43 -0400

    Delete GamePortfolio directory

--- GamePortfolio/js/main.js
+++
@@ -1,9 +0,0 @@
-// dynamically loading header to element header
-window.addEventListener("DOMContentLoaded", () => {
-  fetch("/components/header.html")
-    .then(res => res.text())
-    .then(data => {
-      document.getElementById("header").innerHTML = data;
-    })
-    .catch(err => console.error("Failed to load header:", err));
-});
